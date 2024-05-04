153b29f4bf83bd8d8600f9d3fd7f7dfb1e5ae313
Marcio Curvello <marciocurvello@gmail.com>
2024-05-04 02:07:45 -0300

    feat: add put stocks request

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fe5d0982d63541065e0adc679cea5ea3eb73ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693167fa18b4a4d15ff59571dd56d647e9752bdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
