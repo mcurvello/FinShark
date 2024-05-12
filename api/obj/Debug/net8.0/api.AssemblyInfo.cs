ea6d625f33ea4a1f728f11f350e8612318fabc70
Marcio Curvello <marciocurvello@gmail.com>
2024-05-12 11:42:45 -0300

    refactor: update relationship

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809f0ebc49c8ad1e80a548efbda26e07dd288d70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa83ad60f09adcae67a674ae26891667b61be35d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
