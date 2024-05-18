8176a1b01f83f16b04233cc378e62eb5473638ac
Marcio Curvello <marciocurvello@gmail.com>
2024-05-18 15:02:04 -0300

    feat: lists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import { searchCompanies } from './api';
 function App() {
   const [search, setSearch] = useState<string>("");
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
+  const [serverError, setServerError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -28,7 +28,8 @@ function App() {
   return (
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange} />
-      <CardList />
+      {serverError && <h1>{serverError}</h1>}
+      <CardList searchResults={searchResult} />
     </div>
   );
 }
