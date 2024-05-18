8680515d937f92b0107501fa2a07676c965fd315
Marcio Curvello <marciocurvello@gmail.com>
2024-05-18 10:38:59 -0300

    refactor: update with event handlers

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -3,12 +3,12 @@ import Card from '../Card/Card'
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList: React.FC<Props>= (props: Props): JSX.Element => {
   return (
     <div>
       <Card companyName='Apple' ticker='AAPL' price={100} />
       <Card companyName='Microsoft' ticker='MSFT' price={200} />
-      <Card companyName='TESLA' ticker='TSLA' price={300} />
+      <Card companyName='Tesla' ticker='TSLA' price={300} />
     </div>
   )
 }
---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,25 @@
+import React, { ChangeEvent, SyntheticEvent, useState } from 'react'
+
+type Props = {}
+
+const Search: React.FC<Props> = (props: Props): JSX.Element => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e)
+  };
+
+  return (
+    <div>
+      <input type="text" value={search} onChange={(e) => handleChange(e)} />
+      <button onClick={(e) => onClick(e)} />
+    </div>
+  )
+}
+
+export default Search;
